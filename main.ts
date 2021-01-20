86253b173f3a781d15c1cea71157e04ea7807d00
Firo <mail@firo.it>
2021-01-20 14:28:21 +0100

    add line tracking

--- main.ts
+++ main.ts
@@ -4,14 +4,22 @@ basic.forever(function () {
     distance = maqueen.Ultrasonic(PingUnit.Centimeters)
     if (distance < 4) {
         speed = 0
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
     } else if (distance > 30) {
         speed = 255
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     } else {
         speed = distance * 7
     }
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
+})
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
